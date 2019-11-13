bb502aab620c592c093f076e56c398a6d7514e4b
sprinzs123 <49489162+sprinzs123@users.noreply.github.com>
2019-11-12 19:29:52 -0600

    Update settings.py

--- bakery/settings.py
+++ bakery/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'bakery/static/')
 ]
