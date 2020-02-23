f12d7cce75f1c2f26729061cc0720b8b5d276015
Your Name <you@example.com>
2020-02-22 21:06:59 -0600

    modal items added

--- store/static/store/js/script.js
+++ store/static/store/js/script.js
@@ -3,9 +3,11 @@ itemFilter()
 nameSearch()
 addToCart()
 modalItemShow()
-showCost()
 showHowMany()
+showHowMuch()
 modalExpand()
+deleteItem()
+formValidation()
 // click event on the filter btns
 function itemFilter(){
     allFilterBtns = document.querySelectorAll('.filter-btn')
@@ -83,7 +85,7 @@ function addToCart(){
   })
 }
 
-// #### display item from local storage into modal //
+// #### display item from local storage into modal  and popover//
 
 // getting information for each item from cart and passing it to other function
 function modalItemShow(){
@@ -98,17 +100,18 @@ function modalItemShow(){
     let itemQuantity = itemDetailList[0]
     let itemImgSrc = itemDetailList[3]
     let itemPrice = itemDetailList[2]
-
-    addItemModal(itemName, itemQuantity, itemImgSrc, itemPrice)
+    let itemId = item
+    addItemModal(itemName, itemQuantity, itemImgSrc, itemPrice, itemId)
+    orderSummary(itemName, itemQuantity, itemImgSrc, itemPrice)
   }
 }
-// add items dynamically to modal
+// add items dynamically to modal/popover
 // relies on modalItemShow() function
-function addItemModal(name, quantity, imgSrc, price){
+function addItemModal(name, quantity, imgSrc, price, itemId){
   let totalPrice = price * quantity
   let parentDiv = document.getElementById('modal-items')
   newItem = `
-  <div class='row justify-content-around my-1 pl-2'>
+  <div class='row justify-content-around my-1 pl-2' id='${itemId}'>
     <div class="col-4">
         <img src="${imgSrc}" alt="omg" class='img-fluid my-auto'>
     </div>
@@ -121,10 +124,37 @@ function addItemModal(name, quantity, imgSrc, price){
     <div class="col-2 my-auto">
         <i class="fas fa-trash"></i>
     </div>
-    </div>
+  </div>
     `
   createElement(parentDiv, newItem)
 }
+
+// add items to pop screen/modal
+// use local storage to fetch items
+function orderSummary(name, quantity, imgSrc, price){
+  let parentDiv = document.querySelector('.order-items')
+  let totalPrice = price * quantity
+  newItem = `
+  <div class='row justify-content-around my-1 pl-2'>
+    <div class="col-3">
+        <img src="${imgSrc}" alt="omg" class='img-fluid my-auto'>
+    </div>
+    <div class="col-3 my-auto">
+        <h6 class=''>${name}</h6> 
+    </div>
+    <div class='col-2 my-auto'>
+      <h6><span class=''>Qty: ${quantity}</span></h6>
+    </div>  
+    <div class='col-2 my-auto'>
+        <h6>$<span class='item-price-modal'>${totalPrice}</span></h6>
+    </div>  
+  </div>
+  `
+  createElement(parentDiv, newItem)
+}
+
+
+
 // create element and add it to designed parent container
 function createElement(parent, newItem){
   let itemTry = document.createElement("div");
@@ -148,14 +178,6 @@ function totPrice(){
   }
   return cost
 }
-// show total cost at front page
-function showCost(){
-  let cost = totPrice()
-  let modalTotal = document.querySelector('.modal-total-price')
-  let navTotal = document.querySelector('.item-total')
-  navTotal.innerHTML = cost
-  modalTotal.innerHTML = cost
-}
 
 // calculate number of items using modal
 function countItems(){
@@ -174,12 +196,97 @@ function showHowMany(){
   let navCount = document.querySelector('#item-count')
   navCount.innerHTML = number
 }
+function showHowMuch(){
+  let total = totPrice()
+  let displayedPrice = document.querySelector('.item-total')
+  displayedPrice.innerHTML = total
+}
+
 
 // expand/dropdown modal
 function modalExpand(){
-  let modalToggleBtn = document.querySelector('#cart-info')
-  let cart = document.querySelector('.cart')
-  modalToggleBtn.addEventListener('click', function(event){
+  let popoverContainers = document.getElementById('modal-items')
+  if(popoverContainers.hasChildNodes()){
+    console.log(popoverContainers.childCount)
+    let modalToggleBtn = document.querySelector('#cart-info')
+    let cart = document.querySelector('.cart')
+    modalToggleBtn.addEventListener('click', function(event){
     cart.classList.toggle('modal-expand')
+    })
+  }
+
+}
+
+// delete item from popover at click of trashcan btn
+// delete from local storage and template
+function deleteItem(){
+  let deleteBtn = document.querySelectorAll('.fa-trash')
+  deleteBtn.forEach(function(btn){
+    btn.addEventListener('click', function(event){
+      let itemRow =btn.parentElement.parentElement
+      let itemId = itemRow.id
+      let popParent = itemRow.parentElement
+      popParent.removeChild(itemRow)
+
+      // remove item from local storage
+      let cartItems = localStorage.getItem("cart");
+      let cartDic = JSON.parse(cartItems);
+      delete cartDic[itemId];
+      localStorage.setItem("cart", JSON.stringify(cartDic));
+
+      showHowMany()
+      showHowMuch()
   })
+  })
+}
+
+// form validation when placing order
+// checking field if they are correct in the form...fun
+function formValidation(){
+  let errors = 0;
+  let errorMsg = document.getElementById('card-form-error')
+
+  let contactNameBox = document.getElementById('name')
+  let contactPhoneBox = document.getElementById('phone-number')
+  let nameBox = document.getElementById("cardName");
+  let cardNumBox = document.getElementById("cardNum");
+  let monthBox = document.getElementById("month");
+  let yearBox = document.getElementById("year");
+  let codeBox = document.getElementById("code");
+
+  let contactName = document.querySelector('#name')
+  let contactPhone = document.querySelector('#phone-number')
+  let name = document.querySelector("#cardName").value;
+  let cardNum = document.querySelector("#cardNum").value;
+  let month = document.querySelector("#month").value;
+  let year = document.querySelector("#year").value;
+  let code = document.querySelector("#code").value;
+
+  if (isNaN(year) || year.length > 5 || year.includes(".")) {
+    errorMsg.style.display = "block";
+    yearBox.classList.add("error-border");
+    errors += 1;
+  }
+  if (isNaN(month) || parseInt(month) > 12 || month.includes(".")) {
+    errorMsg.style.display = "block";
+    monthBox.classList.add("error-border");
+    errors += 1;
+  }
+  if (isNaN(cardNum) || cardNum.length != 3 || cardNum.includes(".")) {
+    errorMsg.style.display = "block";
+    cardNumBox.classList.add("error-border");
+    errors += 1;
+  }
+  if (isNaN(code) || code.length != 3 || code.includes(".")) {
+    errorMsg.style.display = "block";
+    codeBox.classList.add("error-border");
+    errors += 1;
+  }
+  if (/^[A-Za-z]+$/.test(name.replace(" ", "")) == false) {
+    errorMsg.style.display = "block";
+    nameBox.classList.add("error-border");
+    errors += 1;
+  }
+
+
 }
